4a5232e447bbeeec7b632fc910958496e922fa93
Oliver Munn <oliver_munn@icloud.com>
2023-10-24 14:23:08 +0100

    Migrated notificaitons

--- src/controllers/notifications.ts
+++ src/controllers/notifications.ts
@@ -2,54 +2,32 @@ import express, { Router, Request, Response } from "express"
 import middleware from "./middleware";
 import mongoose from "../db/connection";
 import CustomRequest from "./CustomRequest";
+import prisma from "../db/postgres";
+import { Worker } from 'snowflake-uuid'; // Import a unique ID generator library
 
+
+
+// Create a unique ID generator instance
+const generator = new Worker(0, 1, {
+  workerIdBits: 5,
+  datacenterIdBits: 5,
+  sequenceBits: 12,
+});
 const router = Router();
-
 // Endpoint for handling POST requests to '/token'
 router.post('/token', middleware.isLoggedIn, async (req: Request, res: Response) => {
   const _id = BigInt((req as CustomRequest).user._id);
 
-  const { User } = (req as CustomRequest).context.models;
 
   // Start a session with MongoDB
-  const session = await mongoose.startSession();
 
-  session.startTransaction();
   try {
     // Find the user by their _id using the User model and within the session
-    const user = await User.findOne({ "_id": _id }).session(session)
-
-    const fcmToken = req.body.fcm_token as string;
-    const tokens = user["fcm_token"] as string[];
-
-    if (user["fcm_token"]) {
-      // Check if the fcmToken is already present in the user's tokens
-      if (tokens.includes(fcmToken)) {
-        session.endSession();
-        return res.json({ "message": "token already saved" })
-
-      }
-      else {
-        let retval;
-
-        // Update the user by pushing the new fcmToken to the fcm_token array
-        retval = await User.updateOne(
-          { "_id": _id },
-          { $push: { "fcm_token": fcmToken } }
-        ).session(session);
-
-        // Commit the transaction and end the session
-        await session.commitTransaction();
-        session.endSession();
-
-        return res.json(retval);
-      }
-    }
-  } catch (error) {
+    const fcmToken = await prisma.fcm_tokens.upsert({ where: { user_id: _id, token: req.body.fcm_token as string }, create: { token_id: generator.nextId(), user_id: _id, token: req.body.fcm_token }, update: {} });
+    return res.json(fcmToken);
+  }
+  catch (error) {
     // If an error occurs, abort the transaction, end the session, and send an error response
-    await session.abortTransaction();
-    session.endSession();
-
     res.status(400).json({ error });
   }
 });
