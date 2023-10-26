b994cea10747dda84b95b1e32377c41995d88fcf
Oliver Munn <oliver_munn@icloud.com>
2023-10-26 20:01:01 +0100

    Added minor input validate & better retvals 4 user

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -6,7 +6,7 @@ import middleware from "./middleware"; // Import custom middleware
 import CustomRequest from "./CustomRequest"; // Import a custom request type
 import prisma from "../db/postgres"; // Import Prisma ORM for database operations
 import { Worker } from 'snowflake-uuid'; // Import a unique ID generator library
-
+import validator from 'validator';
 const router = Router(); // Create a router to create a route bundle
 
 // Destructure environment variables with defaults
@@ -18,6 +18,7 @@ const generator = new Worker(0, 1, {
   datacenterIdBits: 5,
   sequenceBits: 12,
 });
+const ec = "error_code";
 
 // User Authentication Endpoints
 // These endpoints are used to authorize and authenticate users
@@ -25,6 +26,20 @@ const generator = new Worker(0, 1, {
 // Signup route to create a new user
 router.post("/signup", async (req: any, res) => {
   //  try {
+  const isEmail = validator.isEmail(req.body.email);
+  const isValidPassword = validator.isLength(req.body.password, { min: 8, max: 100 })
+  const isValidUsername = validator.isLength(req.body.username, { min: 4, max: 24 })
+  if (!isEmail) {
+    return res.status(400).json({ ec: 0x001 })
+  }
+  else if (!isValidPassword) {
+    return res.status(400).json({ ec: 0x002 })
+  }
+  else if (!isValidUsername) {
+    return res.status(400).json({ ec: 0x003 })
+  }
+
+
   // Hash the user's password
   let passwordHash = await bcrypt.hash(req.body.password, 10);
   // Generate a unique user ID
@@ -47,12 +62,8 @@ router.post("/signup", async (req: any, res) => {
       }
     },
   });
-
-  // Log a success message
-  console.log('User and email verification created successfully.');
-
   // Send the newly created user as a response
-  res.json(data);
+  res.status(201).json({ "success": true });
   // } catch (error) {
   //   // If an error occurs, send a 400 Bad Request response with the error
   //   res.status(400).json({ error });
@@ -62,6 +73,16 @@ router.post("/signup", async (req: any, res) => {
 // Login route to verify a user and issue a token
 router.post("/login", async (req: any, res) => {
   try {
+    const isValidPassword = validator.isLength(req.body.password, { min: 8, max: 100 })
+    const isValidUsername = validator.isLength(req.body.username, { min: 4, max: 24 })
+    if (!isValidPassword) {
+      return res.status(400).json({ ec: 0x004 })
+    }
+    else if (!isValidUsername) {
+      return res.status(400).json({ ec: 0x005 })
+    }
+
+
     // Check if the user exists
     const user = await prisma.users.findFirst({ where: { username: req.body.username } })
     if (user) {
@@ -70,12 +91,12 @@ router.post("/login", async (req: any, res) => {
       if (result) {
         // Sign a token and send it in the response
         const token = await jwt.sign({ username: user.username, _id: user.user_id }, SECRET);
-        res.json({ token });
+        return res.status(200).json({ token });
       } else {
-        res.status(400).json({ error: "Password doesn't match" });
+        res.status(400).json({ ec: 0x007 });
       }
     } else {
-      res.status(400).json({ error: "User doesn't exist" });
+      res.status(400).json({ ec: 0x006 });
     }
   } catch (error) {
     res.status(400).json({ error });
@@ -93,10 +114,10 @@ router.post("/description", middleware.isLoggedIn, async (req: any, res) => {
 
   try {
     // Update the user's description in the database
-    let t = await prisma.users.update({ where: { user_id: _id }, data: { description: req.body.description } })
+    const updatedUser = await prisma.users.update({ where: { user_id: _id }, data: { description: req.body.description } })
 
     // Return the response from the database update
-    res.json(t)
+    res.status(200).json({ "success": true })
   } catch (error) {
     // If there is an error, return a 400 status code and the error message
     res.status(400).json({ error });
@@ -110,10 +131,10 @@ router.post("/avatar", middleware.isLoggedIn, async (req: any, res) => {
 
   try {
     // Update the user's avatar in the database
-    let t = await prisma.users.update({ where: { user_id: _id }, data: { avatar_id: req.body.avatar } })
+    const updatedUser = await prisma.users.update({ where: { user_id: _id }, data: { avatar_id: req.body.avatar } })
 
     // Return the response from the database update
-    res.json(t)
+    res.status(200).json({ "success": true })
   } catch (error) {
     // If there is an error, return a 400 status code and the error message
     res.status(400).json({ error });
@@ -127,10 +148,10 @@ router.post("/email", middleware.isLoggedIn, async (req: any, res) => {
 
   try {
     // Update the user's email in the database (TODO: Redo Email Verification)
-    let t = await prisma.users.update({ where: { user_id: _id }, data: { email: req.body.description } })
+    const updatedUser = await prisma.users.update({ where: { user_id: _id }, data: { email: req.body.description } })
 
     // Return the response from the database update
-    res.json(t)
+    res.status(200).json({ "success": true });
   } catch (error) {
     // If there is an error, return a 400 status code and the error message
     res.status(400).json({ error });
@@ -144,10 +165,45 @@ router.get("/", middleware.isLoggedIn, async (req: any, res) => {
 
   try {
     // Find the user in the database
-    const user = await prisma.users.findFirst({ where: { user_id: req.headers.id != "0" ? BigInt(req.headers.id) : _id } })
+    const user = await prisma.users.findUnique({ where: { user_id: req.headers.id != "0" ? BigInt(req.headers.id) : _id } })
+    if (user) {
+      let returnUser = {}
+      // Return the response from the database query
+      if (user.user_id == _id) {
+        returnUser = {
+          "user_id": user.user_id,
+          "avatar_id": user.avatar_id,
+          "description": user.description,
+          "email": user.email,
+          "email_notifications": user.email_notifications,
+          "dyslexia_font": user.dyslexia_font,
+          "public_profile": user.public_profile,
+          "hide_location": user.hide_location,
+          "analytics_enabled": user.analytics_enabled,
+          "background": user.background,
+          "country": user.country,
+          "username": user.username,
+          "display_name": user.display_name,
+          "user_role": user.user_role
+        }
+      }
+      else {
+        returnUser = {
+          "user_id": user.user_id,
+          "avatar_id": user.avatar_id,
+          "description": user.description,
+          "public_profile": user.public_profile,
+          "country": user.country,
+          "username": user.username,
+          "display_name": user.display_name,
+          "user_role": user.user_role
+        }
+      }
+      return res.status(200).json(returnUser);
+    } else {
+      res.status(400).json({ ec: 0x006 });
+    }
 
-    // Return the response from the database query
-    res.json(user)
   } catch (error) {
     // If there is an error, return a 400 status code and the error message
     res.status(400).json({ error });
@@ -170,7 +226,7 @@ router.get("/follows", middleware.isLoggedIn, async (req: any, res) => {
         return res.json({ "following": false, "requested": true });
       }
       else {
-        return res.json({ "following": false });
+        return res.json({ "following": false, "requested": false });
       }
     }
   } catch (error) {
